21b6e794993832332ac8ea865e9b05c9e2036d6b
michaeladilulo <michaeladilulo@gmail.com>
2024-08-19 23:05:48 -0600

    Adds React-Modal, styles and works on balance form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
 import NavBar from './components/NavBar/NavBar';
+import BalanceForm from './components/BalanceForm/BalanceForm'
+import './App.css'
 
 function App() {
 
@@ -9,7 +10,17 @@ function App() {
       <div className='application-container'>
         <div className='application-container-elements'>
           <h2>Best Budget Tracker In Tech</h2>
-          <h3>Expense Tracker</h3>
+          <div className='application-container-subheader'>
+            <h3>Expense Tracker</h3>
+          </div>
+            <div className='modal-balance-form-container'>
+              <div className='budget-amount-container'>
+                <p className='budget-amount-title'>Budget:</p>
+                <span className='budget-amount'>$</span><span className='budget-amount'>0</span>
+              </div>
+              
+              <BalanceForm />
+            </div>
         </div>
       </div>
     </>
