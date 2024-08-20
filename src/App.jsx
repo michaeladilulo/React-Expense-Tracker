4fb7b8b5b115c400238b7290d434a6f10ce94045
michaeladilulo <michaeladilulo@gmail.com>
2024-08-19 22:29:31 -0600

    Simple Navbar setup for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <NavBar />
     </>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+            <header className='navigation-container'> 
+                <nav className='navigation-elements-container'>
+                    <h1 className='navigation-element'>Expense Tracker</h1>
+                </nav>
+            </header>
+    )
+}
+
+export default NavBar;
